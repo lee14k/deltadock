7c20f1cdddab8c05376bfaea09e0e6e8c34ad2c0
lee14k <kmkovach@mtu.edu>
2024-05-19 20:07:56 -0400

    adding home landing components

---
+++ components/ArchShape.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ArchShape = () => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 491.887 491.887">
+    <g clipPath="url(#clip1)">
+      <path 
+        fill="#4d4b44" 
+        d="M 88.828125 133.28125 L 403.058594 133.28125 L 403.058594 478.457031 L 88.828125 478.457031 Z M 88.828125 133.28125"
+        fillOpacity="1"
+        fillRule="nonzero"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip1">
+        <path d="M0 0h491.887v491.887H0z"/>
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default ArchShape;
