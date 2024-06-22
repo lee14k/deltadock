3676d624bda18911d02bc7f47eea1bfc757dfc33
lee14k <kmkovach@mtu.edu>
2024-06-22 12:21:47 -0400

    icon bar

---
+++ components/ThreeColIcon.jsx
@@ -0,0 +1,23 @@
+export default function ThreeColIcon() {
+  const groups = [
+    "./garage-door-icon-one.png",
+    "./garage-door-icon-two.png",
+    "./garage-door-icon-three.png",
+  ]; 
+
+  return (
+    <div className="bg-blue-800">
+        <h1>We have you covered!</h1>
+        <p>Copy</p>
+        <div className="grid grid-cols-3 ">
+      {groups.map((icon, index) => (
+        <div key={index}>
+          <div className=""></div>
+          <img src={icon} alt={`Icon ${index + 1}`} />
+          <h2>Corresponding Text</h2>
+        </div>
+      ))}
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import ThreeCol from "@/components/ThreeCol";
 import HomeHeader from "../components/HomeHeader";
+import ThreeColIcon from "@/components/ThreeColIcon";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -9,7 +10,7 @@ export default function Home() {
     <div>
     <HomeHeader/>
     <ThreeCol/>
-
+    <ThreeColIcon/>
     </div>
   );
 }
