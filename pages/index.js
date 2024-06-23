7843d307743e4fe218728cdd9c9fdedeb46ea014
lee14k <kmkovach@mtu.edu>
2024-06-23 10:08:32 -0400

    installing a bunch of packages

--- pages/index.js
+++ pages/index.js
@@ -5,11 +5,13 @@ import HomeHeader from "../components/HomeHeader";
 import ThreeColIcon from "@/components/ThreeColIcon";
 import LeftPhoto from "@/components/LeftPhoto";
 import RightPhoto from "@/components/RightPhoto";
+import Navbar from "@/components/FrontEnd/Navbar";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <div>
+      <Navbar/>
       <HomeHeader />
       <ThreeCol />
       <ThreeColIcon />
---
+++ routes.js
@@ -0,0 +1,8 @@
+export const routes = [
+    { title: "Home", href: "/" },
+    {title: "Our Team", href: "/book"},
+    {title:"Services", href:"/chefs-table"}, 
+    { title: "Contact", href: "/contact" },
+  
+  ];
+  
