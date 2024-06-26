74d50e6f70057ce091df313f034d0bd60529c96c
lee14k <kmkovach@mtu.edu>
2024-06-26 07:08:02 -0400

    contact form fixes

--- components/FrontEnd/ContactFill.jsx
+++ components/FrontEnd/ContactFill.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import { Switch } from "@headlessui/react";
 import Link from "next/link";
 import { Libre_Caslon_Display } from "next/font/google";
 
 const libre = Libre_Caslon_Display({ subsets: ["latin"], weight: "400" });
+
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
@@ -48,167 +48,82 @@ export default function ContactFill() {
       console.error("There was an error submitting the form:", error);
     }
   };
+
   const closeModal = () => {
     setIsModalOpen(false);
   };
 
   return (
-    <div className="isolate px-6 py-24 sm:py-32 lg:px-8 grid lg:grid-cols-2 customcontact ">
-      <div className="mx-auto max-w-2xl grid bg-white px-48 text-left">
-        <h2
-          className={`text-4xl font-bold tracking-tight text-gray-900 sm:text-7xl py-10 ${libre.className} `}
-        >
+    <div className="isolate px-6 py-24 sm:py-32 lg:px-8 grid lg:grid-cols-2 customcontact">
+      <div className="mx-auto  grid bg-white px-48 py-12 lg:px-12 text-left">
+        <h2 className={`text-4xl font-bold tracking-tight text-gray-900 sm:text-7xl py-10 ${libre.className}`}>
           Contact Us
         </h2>
-<div className="flex flex-col text-left my-2"><h3>Address</h3>
-<span>PO Box 36</span>
-<span>Rapid River, MI 49878</span>
-</div>
-<div className="flex flex-col text-left my-2"><h3>Phone</h3>
-<span>Office 906.428.1294</span>
-<span>Cory 906.399.0126</span>
-<span>Katrina 906.399.0891</span>
-
-</div>
-<div className="flex flex-col text-left my-2"><h3>Email</h3>
-<span>deltadockequipment@gmail.com</span>
-
-</div>
-<h4>Find us on Facebook!</h4>
-
-
-
-        <p> Questions? Call (906)-221-5371</p>
-        <button>Call us today</button>
+        <div className="flex flex-col my-2">
+          <h3 className="text-lg font-semibold text-gray-900">Address</h3>
+          <span>PO Box 36</span>
+          <span>Rapid River, MI 49878</span>
+        </div>
+        <div className="flex flex-col my-2">
+          <h3 className="text-lg font-semibold text-gray-900">Phone</h3>
+          <span>Office 906.428.1294</span>
+          <span>Cory 906.399.0126</span>
+          <span>Katrina 906.399.0891</span>
+        </div>
+        <div className="flex flex-col my-2">
+          <h3 className="text-lg font-semibold text-gray-900">Email</h3>
+          <span>deltadockequipment@gmail.com</span>
+        </div>
+        <h4 className="text-lg font-semibold text-gray-900 mt-4">Find us on Facebook!</h4>
+        <p className="text-lg text-gray-900">Questions? Call (906)-221-5371</p>
+        <button className="mt-2 bg-sky-600 text-white py-2 px-4 rounded hover:bg-sky-500">Call us today</button>
       </div>
-      <form
-        method="POST"
-        className="mx-auto mt-16 max-w-xl sm:mt-20 bg-white"
-        onSubmit={handleSubmit}
-      >
+
+      <form method="POST" className="mx-auto mt-16 max-w-2xl sm:mt-20 bg-white px-6 lg:px-12" onSubmit={handleSubmit}>
         <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
           <div>
-            <label
-              htmlFor="firstName"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              First name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="firstName"
-                id="firstName"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            <label htmlFor="firstName" className="block text-sm font-semibold leading-6 text-gray-900">First name</label>
+            <input type="text" name="firstName" id="firstName" autoComplete="given-name"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
           <div>
-            <label
-              htmlFor="lastName"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Last name
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="text"
-                name="lastName"
-                id="lastName"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            <label htmlFor="lastName" className="block text-sm font-semibold leading-6 text-gray-900">Last name</label>
+            <input type="text" name="lastName" id="lastName" autoComplete="family-name"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
           <div className="sm:col-span-2">
-            <label
-              htmlFor="email"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Email
-            </label>
-            <div className="mt-2.5">
-              <input
-                type="email"
-                name="email"
-                id="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            <label htmlFor="email" className="block text-sm font-semibold leading-6 text-gray-900">Email</label>
+            <input type="email" name="email" id="email" autoComplete="email"
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
           <div className="sm:col-span-2">
-            <label
-              htmlFor="phoneNumber"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Phone number
-            </label>
-            <div className="relative mt-2.5">
-              <input
-                type="tel"
-                name="phoneNumber"
-                id="phoneNumber"
-                autoComplete="tel"
-                className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
+            <label htmlFor="phoneNumber" className="block text-sm font-semibold leading-6 text-gray-900">Phone number</label>
+            <input type="tel" name="phoneNumber" id="phoneNumber" autoComplete="tel"
+              className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" />
           </div>
           <div className="sm:col-span-2">
-            <label
-              htmlFor="message"
-              className="block text-sm font-semibold leading-6 text-gray-900"
-            >
-              Message
-            </label>
-            <div className="mt-2.5">
-              <textarea
-                name="message"
-                id="message"
-                rows={4}
-                className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                defaultValue={""}
-              />
-            </div>
+            <label htmlFor="message" className="block text-sm font-semibold leading-6 text-gray-900">Message</label>
+            <textarea name="message" id="message" rows={4}
+              className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" defaultValue={""} />
           </div>
-          <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-            <div className="flex h-6 items-center">
-              <Switch
-                checked={agreed}
-                onChange={setAgreed}
-                className={classNames(
-                  agreed ? "bg-indigo-600" : "bg-gray-200",
-                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                )}
-              >
+          <div className="sm:col-span-2 flex items-center">
+            <Switch.Group as="div" className="flex gap-x-4">
+              <Switch checked={agreed} onChange={setAgreed}
+                className={classNames(agreed ? "bg-indigo-600" : "bg-gray-200",
+                  "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600")}>
                 <span className="sr-only">Agree to policies</span>
-                <span
-                  aria-hidden="true"
-                  className={classNames(
-                    agreed ? "translate-x-3.5" : "translate-x-0",
-                    "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                  )}
-                />
+                <span aria-hidden="true" className={classNames(agreed ? "translate-x-3.5" : "translate-x-0",
+                  "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out")} />
               </Switch>
-            </div>
-            <Switch.Label className="text-sm leading-6 text-gray-600">
-              By selecting this, you agree to our{" "}
-              <Link
-                href="/privacy-policy"
-                className="font-semibold text-sky-600"
-              >
-                privacy&nbsp;policy
-              </Link>
-              .
-            </Switch.Label>
-          </Switch.Group>
+              <Switch.Label className="text-sm leading-6 text-gray-600">
+                By selecting this, you agree to our{" "}
+                <Link href="/privacy-policy" className="font-semibold text-sky-600">privacy&nbsp;policy</Link>.
+              </Switch.Label>
+            </Switch.Group>
+          </div>
         </div>
         <div className="mt-10">
-          <button
-            type="submit"
-            className="block w-full rounded-md bg-sky-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
-          >
+          <button type="submit" className="block w-full rounded-md bg-sky-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600">
             Let's talk
           </button>
         </div>
@@ -220,10 +135,7 @@ export default function ContactFill() {
                 Submission Successful!
               </p>
               <p>Your submission was successful. Thank you!</p>
-              <button
-                onClick={closeModal}
-                className="mt-4 bg-red-600 text-white py-2 px-4 rounded hover:bg-red-500"
-              >
+              <button onClick={closeModal} className="mt-4 bg-red-600 text-white py-2 px-4 rounded hover:bg-red-500">
                 Close
               </button>
             </div>
