845ded97d65b21edaf203713fc194593bf1b377f
lee14k <kmkovach@mtu.edu>
2024-10-04 11:10:17 -0400

    tweaks for mobile

--- pages/_document.js
+++ pages/_document.js
@@ -2,16 +2,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
-        />
-      <body className="overflow-x-hidden">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+      <Html lang="en">
+          <Head/>
+          <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+          />
+          <meta name="apple-mobile-web-app-capable" content="yes"/>
+
+          <body className="overflow-x-hidden w-full relative">
+          <Main/>
+          <NextScript/>
+          </body>
+      </Html>
   );
 }
