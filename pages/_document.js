f56325c7fb789048b7e6bfe1933b8769dd4f1549
lee14k <kmkovach@mtu.edu>
2024-10-04 10:59:56 -0400

    trying to hide overflow

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default function Document() {
           name="viewport"
           content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
-      <body className="overflow-x-hidden	">
+      <body className=" overflow-hidden">
         <Main />
         <NextScript />
       </body>
